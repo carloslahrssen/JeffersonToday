4ded2a850ef7cb5a82258aa9f4fa65a885303fcd
carloslahrssen <carloslahrssen@gmail.com>
2016-10-31 14:11:18 -0400

    images
    made changes to background images-static

--- JeffersonToday/wp-content/themes/jeffersontoday/page.php
+++ JeffersonToday/wp-content/themes/jeffersontoday/page.php
@@ -4,13 +4,18 @@
 <?php
     if ( have_posts() ) :
           while(have_posts()) : the_post();?>
-            <article class="post">
-                <h1><?php the_title();?></h1>
-                <p>
-                    <?php the_post_thumbnail('banner-image'); ?>
-                    <?php the_content(); ?>
-                </p>
-            </article>
+            <div class="card-page">
+                <div class="card-container-page">
+                    <article class="post">
+                        <h1><?php the_title();?></h1>
+                        <p>
+                            <?php the_post_thumbnail('banner-image'); ?>
+                            <?php the_content(); ?>
+                        </p>
+                    </article>
+                </div>
+            </div>
+
 <?php
         endwhile;
         endif;
